e20d5d9f5d7d7b7b4ec17451f2d311c728d648e3
Wyatt Habinski <whabinski@hotmail.com>
2025-10-01 01:44:57 -0400

    readded index.css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import "./index.css";
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
