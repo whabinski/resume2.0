1ca61575767d553295feca0675c20625590f26c6
Wyatt Habinski <whabinski@hotmail.com>
2025-10-01 01:54:49 -0400

    updated routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
-import "./index.css";
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
+
 
 
 
